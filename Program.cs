2c55ec003ee6dbc589bf4c2ad46708076d536bf6
SevakAbr <sevak.abrahamyan.99@gmail.com>
2021-02-12 16:56:42 +0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ACA_Homework_1_4
+{
+    class Program
+    {
+        static void f(string str)
+        {
+            string[] splitList = str.Split(new char[] { ' ', ',', '.' });
+            string[] NewString = new string[splitList.Length];
+
+            for (int i = 0; i < splitList.Length; i++)
+            {
+                if (splitList[i] != "")
+                {
+                    char first = splitList[i][0];
+                    NewString[i] = splitList[i].Substring(1, splitList[i].Length - 1) + splitList[i].Substring(0, 1) + "ey";
+                }
+            }
+            for (int i = 0; i < NewString.Length; i++)
+            {
+                Console.Write(NewString[i] + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string please!");
+            string str = Console.ReadLine();
+            f(str);
+        }
+    }
+}
